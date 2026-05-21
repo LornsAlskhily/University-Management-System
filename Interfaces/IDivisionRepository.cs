93eac7491318ba64e45208d759e43c0b599fbfe8
LornsAlskhily <lorns.alskhily@gmail.com>
2026-05-21 21:03:13 +0300

    .gitignore

--- Interfaces/IDivisionRepository.cs
+++ Interfaces/IDivisionRepository.cs
@@ -6,7 +6,7 @@ namespace UniversitySystem.Interfaces
     public interface IDivisionRepository
     {
         List<Divisions> GetAllDivisions();
-        Divisions GetDivisionById(int id, string courseId);
+        Divisions GetDivisionById(int id,string courseId);
         bool AddDivision(Divisions divisons);
     }
 }
